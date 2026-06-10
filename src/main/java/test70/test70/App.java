8ed9e56fe0057066516897151267976e7b448a3e
JorgePerez-dev <jperezaranaz@gmail.com>
2026-06-10 20:51:49 +0200

    first commit

---
+++ src/main/java/test70/test70/App.java
@@ -0,0 +1,13 @@
+package test70.test70;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
